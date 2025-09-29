2fe7d0a4d101fe46a589a88cb65511ecbc8b3513
trungnguyen2411 <https://github.com/trungnguyen2411>
2025-09-29 21:59:16 +0700

    changes

--- web.py
+++ web.py
@@ -6,8 +6,11 @@ todos = functions.get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+    if todo.strip() == "":
+        pass
+    else:
+        todos.append(todo)
+        functions.write_todos(todos)
 
 
 st.title("My Todo App")
